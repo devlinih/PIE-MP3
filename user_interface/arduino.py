96ce856dbbea0686141e4cc5c976711a54d39e8f
Devlin Ih <dih@olin.edu>
2022-10-02 16:40:24 -0400

    Add code to interface with Arduino through Python scripts

---
+++ user_interface/arduino.py
@@ -0,0 +1,53 @@
+"""
+Connect to an Arduino running PIE MP3 Firmware.
+"""
+
+import ast
+import serial
+import serial.tools.list_ports as list_ports
+
+BAUDRATE = 9600
+ARDUINO_TIMEOUT = 3 # seconds
+
+# List of Arduino IDs provided by Brad
+# https://github.com/bminch/PIE/blob/main/Serial_cmd.py
+ARDUINO_IDS = ((0x2341, 0x0043), (0x2341, 0x0001),
+               (0x2A03, 0x0043), (0x2341, 0x0243),
+               (0x0403, 0x6001), (0x1A86, 0x7523))
+
+
+def guess_port():
+    """
+    Try to find an Arduino connected to the computer.
+
+    Returns:
+        A string representing the port of the first Arduino found. Returns the
+        empty string if no Arduino is found.
+    """
+    devices = list_ports.comports()
+    for device in devices:
+        if (device.vid, device.pid) in ARDUINO_IDS:
+            return device.device
+    return ""
+
+
+def send_command(ser: serial.Serial, command: str):
+    """
+    Send a command to an Arduino connected over serial.
+
+    Args:
+        ser: A serial connection to an Arduiono.
+        command: A string representing a command to send to the Arduino.
+
+    Returns:
+        A number representing the response from the Arduino. If the response is
+        invalid, returns 0.
+    """
+    message = bytes(command.strip() + "\n", "utf-8")
+    ser.write(message)
+    data = ser.readline()
+    # data is a bytestring, return it as a number.
+    try:
+        return int(data.decode("utf-8").strip())
+    except(ValueError):
+        return 0
