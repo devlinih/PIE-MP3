5b07b9f82700224d040633efa536ffa7d7a071b8
thegalaxykat <katcanavan@gmail.com>
2022-10-04 17:48:27 -0400

    Add serial commands

--- LineRobotFirmware/LineRobotFirmware.ino
+++ LineRobotFirmware/LineRobotFirmware.ino
@@ -14,29 +14,70 @@ const int RIGHT_SENSE = 0;  // analog pin 0
 const int LEFT_SENSE = 1;   // analog pin 1
 
 // function prototypes for organization
-void serialMonitor();
-void leftSensorRead();
-void rightSensorRead();
+void setWheelLeft();
+void setWheelRight();
+void readSensors();
+void stop();
 
 void setup() {
   // initialize serial communication
   Serial.begin(115200);
+  Serial.print("START");
+
+  // motors always run forwards
+  leftWheel->run(FORWARD);
+  rightWheel->run(FORWARD);
+
+  // set initial motor speeds to 0
+  rightWheel->setSpeed(0);
+  leftWheel->setSpeed(0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// TODO
-void serialMonitor() {
   while (Serial.available() == 0) {
     // wait for data available in serial receive buffer
   }
   String command = Serial.readStringUntil('\n');  // read until timeout
-  command.trim();                                 // remove white space or \n
-  if (command.equals("left_wheel_set")) {
-  } else if (command.indexOf("right_wheel_set") > 0) {
-  } else if (command.indexOf("left_sensor_read") > 0) {
-  } else if (command.indexOf("right_sensor_read") > 0) {
+  if (command.startsWith("setWheelLeft")) {
+    // note: wheel will be in the form "setWheelDirection N" where N is speed
+    // extract speed from command string
+    uint8_t speed = command.substring(command.indexOf(" ")+1, command.length()).toInt();
+    setWheelLeft(speed);
+
+  } else if (command.startsWith("setWheelRight")) {
+    uint8_t speed = command.substring(command.indexOf(" ")+1, command.length()).toInt();
+    setWheelRight(speed);
+
+  } else if (command.equals("readSensors")) {
+    // send sensor readings over serial
+    readSensors();
+  } else if (command.equals("STOP")) {
+    // stop motors
+    stop();
   }
+}
+
+void setWheelLeft(uint8_t speed){
+Serial.println(speed);  // test
+leftWheel->setSpeed(speed);
+}
+void setWheelRight(uint8_t speed){
+Serial.println(speed);  // test
+rightWheel->setSpeed(speed);
+}
+void readSensors(){
+  // read right sensor voltage:
+  int RVal = analogRead(RIGHT_SENSE);
+  // read left sensor voltage:
+  int LVal = analogRead(LEFT_SENSE);
+
+  // print to serial as a tuple
+  char buffer[40];
+  sprintf(buffer, "(%d,%d)", RVal, LVal);
+  Serial.println(buffer);
+}
+void stop(){
+// stop both motors
+leftWheel->setSpeed(0);
+rightWheel->setSpeed(0);
 }
