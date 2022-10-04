3a7c1094934ed35a6564c8bef83103611f8b5f0e
Devlin Ih <dih@olin.edu>
2022-10-04 11:56:16 -0400

    Add template code

---
+++ user_interface/control_loop.py
@@ -0,0 +1,22 @@
+"""
+Control loop for line following robot.
+
+Perhaps add GUI.
+"""
+
+
+def main():
+    # Variables
+    total_speed = 0
+    left_speed  = 0
+    right_speed = 0
+
+    # Control loop
+    # - Read sensors
+    # - Update speeds
+    # - Send new speeds back to firmware
+    pass
+
+
+if __name__ == "__main__":
+    main()
