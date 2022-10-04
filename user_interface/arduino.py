2907485a793727e90caa4a2f0d442a2460853229
Devlin Ih <dih@olin.edu>
2022-10-04 11:27:58 -0400

    Parse tuples from Arduino

--- user_interface/arduino.py
+++ user_interface/arduino.py
@@ -7,7 +7,7 @@ import serial
 import serial.tools.list_ports as list_ports
 
 BAUDRATE = 9600
-ARDUINO_TIMEOUT = 3 # seconds
+ARDUINO_TIMEOUT = 3  # seconds
 
 # List of Arduino IDs provided by Brad
 # https://github.com/bminch/PIE/blob/main/Serial_cmd.py
@@ -16,7 +16,7 @@ ARDUINO_IDS = ((0x2341, 0x0043), (0x2341, 0x0001),
                (0x0403, 0x6001), (0x1A86, 0x7523))
 
 
-def guess_port():
+def guess_port() -> str:
     """
     Try to find an Arduino connected to the computer.
 
@@ -31,7 +31,7 @@ def guess_port():
     return ""
 
 
-def send_command(ser: serial.Serial, command: str):
+def send_command(ser: serial.Serial, command: str) -> str:
     """
     Send a command to an Arduino connected over serial.
 
@@ -47,3 +47,19 @@ def send_command(ser: serial.Serial, command: str):
     data = ser.readline()
     # data is a bytestring, return it as a number.
     return data.decode("utf-8").strip()
+
+
+def parse_tuple(message: str) -> tuple[int, int]:
+    """
+    Process a string in the form '(a,b)' and return a tuple.
+
+    Args:
+        message: A string representing a tuple of ints.
+
+    Returns:
+        A tuple of two ints. If message is invalid, returns the tuple (0, 0).
+    """
+    try:
+        return ast.literal_eval(message)
+    except:
+        return (0, 0)
